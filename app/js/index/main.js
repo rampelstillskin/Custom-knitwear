edee23d90454379caa8e7562d2cb0a9af16f0981
Andrey Kharitonov <rampelstillskin@gmail.com>
2019-08-29 16:54:08 +0300

    added order window, mobile navigation

--- app/js/index/main.js
+++ app/js/index/main.js
@@ -1,3 +1,20 @@
+// МОБИЛЬНОЕ МЕНЮ
+
+const mobileNavBtn = document.querySelector(".mobile-nav__btn"),
+			mobileNavBtnAnimation = mobileNavBtn.querySelector(".mobile-nav__btn-desc"),
+			mobileNav = document.querySelector(".mobile-nav");
+
+const mobileNavClose = () => {
+	mobileNav.classList.add("mobile-nav__show");
+};
+
+mobileNavClose();
+
+mobileNavBtn.addEventListener("click", () => {
+	mobileNav.classList.toggle("mobile-nav__show");
+	mobileNavBtn.classList.toggle("btn-close");
+});
+
 // КАРТА ПОПАП
 
 const mapPopup = document.querySelector(".map-popup"),
@@ -27,4 +44,20 @@ contactsMap.addEventListener("click", () => {
 
 mapPopupBtn.addEventListener("click", () => {
 	closeMap();
+});
+
+// ОКНО ЗАКАЗА
+
+const topProductOrderBtn = document.querySelector(".top-product__order"),
+			modalWindow = document.querySelector(".modal-window"),
+			modalWindowCloseBtn = modalWindow.querySelector(".modal-window__btn-close");
+
+topProductOrderBtn.addEventListener("click", (evt) => {
+	evt.preventDefault();
+	modalWindow.classList.add("modal-window__show");
+});
+
+modalWindowCloseBtn.addEventListener("click", (evt) => {
+	evt.preventDefault();
+	modalWindow.classList.remove("modal-window__show");
 });
