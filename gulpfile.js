d2964d01b57005e1dfbd537e8780a72e75ee70e1
Andrey Kharitonov <rampelstillskin@gmail.com>
2019-11-04 22:50:06 +0300

    поправил таску 'images'

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-const gulp 							= require('gulp');
-const autoprefixer 			= require('gulp-autoprefixer');
-const browserSync 			= require('browser-sync').create();
-const plumber 					= require('gulp-plumber');
-const sass 							= require('gulp-sass');
-const minify						= require('gulp-csso');
-const rename 						= require('gulp-rename');
-const imagemin = require('gulp-imagemin');
-const del								= require('del');
+const gulp 							= require('gulp'),
+			autoprefixer 			= require('gulp-autoprefixer'),
+			browserslist 			= require('browser-sync').create(),
+			plumber 					= require('gulp-plumber'),
+			sass 							= require('gulp-sass'),
+			minify						= require('gulp-csso'),
+			rename 						= require('gulp-rename'),
+			imagemin 					= require('gulp-imagemin'),
+			del								= require('del');
 
 gulp.task('clean', () => del('build'));
 
@@ -17,7 +17,7 @@ gulp.task('sass', () => gulp.src('app/sass/**/*.scss')
     browsers: ['last 5 versions'],
   }))
   .pipe(gulp.dest('app/css'))
-  .pipe(browserSync.stream())
+  .pipe(browserslist.stream())
   .pipe(sass({
     outputStyle: 'compressed' })
     .on('error', sass.logError)));
@@ -27,7 +27,7 @@ gulp.task('minify', () => gulp.src('app/css/style.css')
   .pipe(rename('style.min.css'))
   .pipe(gulp.dest('app/css')));
 
-gulp.task('images', () => gulp.src('app/image/**/*.{png,svg,	jpg}')
+gulp.task('images', () => gulp.src('app/image/**/*.{png,svg,jpg}')
   .pipe(imagemin([
     imagemin.optipng({ optimizationLevel: 3 }),
     imagemin.jpegtran({ progressive: true }),
@@ -36,13 +36,13 @@ gulp.task('images', () => gulp.src('app/image/**/*.{png,svg,	jpg}')
   .pipe(gulp.dest('app/image')));
 
 gulp.task('serve', () => {
-  browserSync.init({
+  browserslist.init({
     server: {
       baseDir: './app',
     },
   });
   gulp.watch('app/sass/**/*.scss', gulp.series('sass'));
-  gulp.watch('app/*.html').on('change', browserSync.reload);
+  gulp.watch('app/*.html').on('change', browserslist.reload);
 });
 
 gulp.task('copy', () => gulp.src([
@@ -57,5 +57,6 @@ gulp.task('copy', () => gulp.src([
   .pipe(gulp.dest('build')));
 
 
-gulp.task('default', gulp.parallel('minify', 'sass', 'serve'));
+gulp.task('optimizeImages', gulp.parallel('images'));
+gulp.task('serve', gulp.parallel('minify', 'sass', 'serve'));
 gulp.task('build', gulp.series('clean', 'copy', 'sass', 'images'));
